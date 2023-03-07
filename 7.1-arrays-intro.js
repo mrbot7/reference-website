b1421294b6c6b50cddcdef639e92c382dc9c8257
mrbot7 <93724741+mrbot7@users.noreply.github.com>
2023-03-07 17:33:35 +0000

    7.1 javascript done

--- 7.1-arrays-intro.js
+++ 7.1-arrays-intro.js
@@ -1,12 +1,23 @@
 function example() {
-  
+                        //0         //1         //2       //3    //4
+  const topStudents = ["Cristian", "Kristian", "Shakka", "Jaime", "AJ"];
+  var name = topStudents[2];
+  console.log(topStudents);
+  topStudents[0] = "Mauricio";
+  console.log(topStudents);
+
 }
 
 function problem1() {
-
+  const travelList = ["France", "Canada", "Japan", "Sweden", "Norway"]
+  var name = travelList[2];
+  console.log(travelList);
 }
 
 
 function problem2(){
-
+  const numList = [2, 3, 5, 7, 11];
+  console.log(numList[0]);
+  console.log(numList[2]);
+  console.log(numList[4]);
 }
