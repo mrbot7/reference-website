4f4391a01ab99ddbe0b1e91039d3e99bd432549d
mrbot7 <93724741+mrbot7@users.noreply.github.com>
2023-03-13 17:10:05 +0000

    nav and 7.3

---
+++ 7.3-array-for-loops.js
@@ -0,0 +1,52 @@
+// Example 1: Print every element in an array
+console.log("Example 1 output below");
+var letters = ["a", "b", "c", "d"];
+
+// Bonus: Define function to print an array passed as a parameter
+
+// Example 2: Print every element in the first half of an array
+console.log("Example 2 output below");
+
+// Example 3: Print every element in the second half of an array
+console.log("Example 3 output below");
+
+// Example 4: Find the sum of all the (numerical) elements in an array
+console.log("Example 4 output below");
+
+// Example 5: Find the max (numerical) value in an array
+console.log("Example 5 output below");
+
+
+// Start function for problem #1
+function problem1(){
+  console.log("Problem 1 output below");
+
+}
+
+
+// Start function for problem #2
+function problem2(){
+  console.log("Problem 2 output below");
+
+}
+
+
+// Start function for problem #3
+function problem3(){
+  console.log("Problem 3 output below");
+
+}
+
+
+// Start function for problem #4
+function problem4(){
+  console.log("Problem 4 output below");
+
+}
+
+
+// Start function for problem #5
+function problem5(){
+  console.log("Problem 5 output below");
+
+}
