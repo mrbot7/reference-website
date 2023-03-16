bbe8e9a3fbae1e7bd66306b2e158feec41760f77
mrbot7 <93724741+mrbot7@users.noreply.github.com>
2023-03-16 18:11:50 +0000

    7.3 probs

--- 7.3-array-for-loops.js
+++ 7.3-array-for-loops.js
@@ -1,7 +1,7 @@
 // Example 1: Print every element in an array
 console.log("Example 1 output below");
 var letters = ["a", "b", "c", "d"];
-for(var i = 0; i < letters.length; i++){
+for (var i = 0; i < letters.length; i++) {
   console.log(letters[i]);
 }
 
@@ -9,7 +9,7 @@ for(var i = 0; i < letters.length; i++){
 
 // Example 2: Print every element in the first half of an array
 console.log("Example 2 output below");
-for (var i = 0; i < letters.length / 2; i++){
+for (var i = 0; i < letters.length / 2; i++) {
   console.log(letters[i]);
 }
 
@@ -18,50 +18,76 @@ console.log("Example 3 output below");
 
 // Example 4: Find the sum of all the (numerical) elements in an array
 console.log("Example 4 output below");
-var nums = [4, 8, 21, 56, 27, 420];
+var nums = [430, 8, 21, 56, 27, 420];
 var sum = 0;
-for (var i = 0; i < nums.length; i++){
+for (var i = 0; i < nums.length; i++) {
   sum += nums[i];
 }
 console.log("Sum = " + sum);
 // Example 5: Find the max (numerical) value in an array
 console.log("Example 5 output below");
-var max;
-for (var i = 0; i < nums.length; i++){
+var max = nums[0];
+for (var i = 0; i < nums.length; i++) {
   var current = nums[i];
-  
+  if (current > max) {
+    max = current;
+  }
+
 }
+console.log("max = " + max);
 
 // Start function for problem #1
-function problem1(){
+var numbers = [1, 8, 3, 4, 2, 9];
+var product = numbers[0];
+function problem1() {
   console.log("Problem 1 output below");
+  for (var i = 0; i < numbers.length; i++) {
+    product = product * numbers[i];
+  }
+  console.log("Product = " + product);
+
 
 }
 
 
 // Start function for problem #2
-function problem2(){
+var cities = ["San Francisco", "New York", "Chicago", "Honolulu"];
+function problem2() {
   console.log("Problem 2 output below");
-
+  for (var i = 0; i < cities.length; i++) {
+    console.log(cities[i]);
+  }
+  console.log(cities);
 }
 
 
 // Start function for problem #3
-function problem3(){
+function problem3() {
   console.log("Problem 3 output below");
-
+  var arr = [1, 2, 3, 4];
+  var doubleArr = doubleList(arr)
+  console.log(doubleArr)
+}
+function doubleArr(){
+  
 }
 
-
 // Start function for problem #4
-function problem4(){
+function problem4() {
   console.log("Problem 4 output below");
 
 }
 
 
 // Start function for problem #5
-function problem5(){
+var arr = [1, 2, 3, 4];
+var reversed = reverseList(arr);
+function problem5() {
   console.log("Problem 5 output below");
+  for (var i = 4; i < arr.length -1; i--){
+
+  }
+  console.log(reversed);
+
 
 }
